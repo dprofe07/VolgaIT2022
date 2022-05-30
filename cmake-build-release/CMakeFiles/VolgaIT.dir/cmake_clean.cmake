1bf12fc98a2a33f6a81fe9b0997a2dab1e8dc3b7
dima <dprofe@yandex.ru>
2022-05-30 22:21:59 +0300

    ..

---
+++ cmake-build-release/CMakeFiles/VolgaIT.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VolgaIT.dir/fairy_tail.cpp.obj"
+  "CMakeFiles/VolgaIT.dir/main.cpp.obj"
+  "VolgaIT.exe"
+  "VolgaIT.exe.manifest"
+  "VolgaIT.pdb"
+  "libVolgaIT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VolgaIT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
